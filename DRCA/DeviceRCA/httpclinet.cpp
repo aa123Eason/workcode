e7633c459ab5ce1ddc2dfa24ce250f8ac1f20ca9
zyc <352852335@qq.com>
2024-01-23 08:45:01 +0800

    开发设备反控外部工具

---
+++ DRCA/DeviceRCA/httpclinet.cpp
@@ -0,0 +1,529 @@
+#include "httpclinet.h"
+#include <QEventLoop>
+
+httpclinet::httpclinet()
+{
+
+}
+
+bool httpclinet::get(QString api,QJsonArray &pJsonArray)
+{
+    QTimer timer;
+    timer.setInterval(300);  // 设置超时时间 30 秒
+    timer.setSingleShot(true);  // 单次触发
+
+    //生成对应的网络请求
+    QNetworkRequest request;
+    QString scheme = SCHEME;
+    QString serverAddr = SERVER_IP;
+    QString port = SERVER_PORT;
+    QString requestHeader = scheme + QString("://") + serverAddr + QString(":") + port;
+    QString fullRequest = requestHeader + QString(api);
+    request.setUrl(QUrl(fullRequest));
+
+    //发送Get请求
+    QNetworkAccessManager manager;
+    QNetworkReply *pReply = manager.get(request);
+
+    QEventLoop loop;
+    QObject::connect(&timer, &QTimer::timeout, &loop, &QEventLoop::quit);
+    QObject::connect(pReply, &QNetworkReply::finished, &loop, &QEventLoop::quit);
+    timer.start();
+    loop.exec(QEventLoop::ExcludeUserInputEvents);  // 启动事件循环
+
+    if (timer.isActive()) {  // 处理响应
+        timer.stop();
+        if (pReply->error() != QNetworkReply::NoError) {
+            // 错误处理
+            qDebug() << "Error String : " << pReply->errorString();
+            return false;
+        } else {
+            QVariant variant = pReply->attribute(QNetworkRequest::HttpStatusCodeAttribute);
+            int nStatusCode = variant.toInt();
+            // 根据状态码做进一步数据处理
+            //QByteArray bytes = pReply->readAll();
+            qDebug() <<__LINE__<<__FUNCTION__<< "Status Code : " << nStatusCode;
+
+            //解析返回的Json结果
+            QByteArray replyData = pReply->readAll();
+            QJsonParseError json_error;
+            QJsonDocument jsonDoc(QJsonDocument::fromJson(replyData, &json_error));
+            if(json_error.error != QJsonParseError::NoError)
+            {
+                return false;
+            }
+            pJsonArray = jsonDoc.array();
+            return true;
+        }
+    } else {  // 处理超时
+        QObject::disconnect(pReply, &QNetworkReply::finished, &loop, &QEventLoop::quit);
+        pReply->abort();
+        pReply->deleteLater();
+        // qDebug() << "Timeout";
+        return false;
+    }
+
+    //eventLoop.exec(QEventLoop::ExcludeUserInputEvents);
+}
+
+bool httpclinet::get(QString api,QJsonObject &pJsonObj)
+{
+    QTimer timer;
+    timer.setInterval(300);  // 设置超时时间 30 秒
+    timer.setSingleShot(true);  // 单次触发
+
+    //生成对应的网络请求
+    QNetworkRequest request;
+    QString scheme = SCHEME;
+    QString serverAddr = SERVER_IP;
+    QString port = SERVER_PORT;
+    QString requestHeader = scheme + QString("://") + serverAddr + QString(":") + port;
+    QString fullRequest = requestHeader + QString(api);
+    request.setUrl(QUrl(fullRequest));
+
+    //发送Get请求
+    QNetworkAccessManager manager;
+    QNetworkReply *pReply = manager.get(request);
+
+    QEventLoop loop;
+    QObject::connect(&timer, &QTimer::timeout, &loop, &QEventLoop::quit);
+    QObject::connect(pReply, &QNetworkReply::finished, &loop, &QEventLoop::quit);
+    timer.start();
+    loop.exec(QEventLoop::ExcludeUserInputEvents);  // 启动事件循环
+
+    if (timer.isActive()) {  // 处理响应
+        timer.stop();
+        if (pReply->error() != QNetworkReply::NoError) {
+            // 错误处理
+            qDebug() << "Error String : " << pReply->errorString();
+            return false;
+        } else {
+            QVariant variant = pReply->attribute(QNetworkRequest::HttpStatusCodeAttribute);
+            int nStatusCode = variant.toInt();
+            // 根据状态码做进一步数据处理
+            //QByteArray bytes = pReply->readAll();
+            qDebug() <<__LINE__<<__FUNCTION__<<"Status Code : " << nStatusCode;
+
+            //解析返回的Json结果
+            QByteArray replyData = pReply->readAll();
+            QJsonParseError json_error;
+            QJsonDocument jsonDoc(QJsonDocument::fromJson(replyData, &json_error));
+            if(json_error.error != QJsonParseError::NoError)
+            {
+                return false;
+            }
+            pJsonObj = jsonDoc.object();
+            return true;
+        }
+    } else {  // 处理超时
+        QObject::disconnect(pReply, &QNetworkReply::finished, &loop, &QEventLoop::quit);
+        pReply->abort();
+        pReply->deleteLater();
+        // qDebug() << "Timeout";
+        return false;
+    }
+}
+
+bool httpclinet::post(QString api,QJsonObject &pJsonObj)
+{
+    QTimer timer;
+    timer.setInterval(300);  // 设置超时时间 30 秒
+    timer.setSingleShot(true);  // 单次触发
+
+    //生成对应的网络请求
+    QNetworkRequest request;
+    QString scheme = SCHEME;
+    QString serverAddr = SERVER_IP;
+    QString port = SERVER_PORT;
+    QString requestHeader = scheme + QString("://") + serverAddr + QString(":") + port;
+    QString fullRequest = requestHeader + QString(api);
+    request.setUrl(QUrl(fullRequest));
+
+    QJsonDocument body(pJsonObj);
+
+    // Connection
+    const QByteArray connection = QByteArrayLiteral("Connection");
+    const QByteArray connectionValue = QByteArrayLiteral("Connection");
+    // Content-Type
+    const QByteArray content = QByteArrayLiteral("Content-Type");
+    const QByteArray contentValue = QByteArrayLiteral("application/json;charset=UTF-8");
+    // token
+    const QByteArray token = QByteArrayLiteral("Authorization");
+    request.setHeader(QNetworkRequest::ContentTypeHeader, contentValue);
+    request.setRawHeader(connection, connectionValue);
+    request.setRawHeader(token, TOKEN);
+    QNetworkAccessManager *Manager = new QNetworkAccessManager();
+    QNetworkReply *pReply = Manager->post(request, body.toJson());
+
+    QEventLoop loop;
+    QObject::connect(&timer, &QTimer::timeout, &loop, &QEventLoop::quit);
+    QObject::connect(pReply, &QNetworkReply::finished, &loop, &QEventLoop::quit);
+    timer.start();
+    loop.exec(QEventLoop::ExcludeUserInputEvents);  // 启动事件循环
+
+    if (timer.isActive()) {  // 处理响应
+        timer.stop();
+        if (pReply->error() != QNetworkReply::NoError) {
+            // 错误处理
+            qDebug() << "Error String : " << pReply->errorString();
+            return false;
+        } else {
+            QVariant variant = pReply->attribute(QNetworkRequest::HttpStatusCodeAttribute);
+            int nStatusCode = variant.toInt();
+            // 根据状态码做进一步数据处理
+            //QByteArray bytes = pReply->readAll();
+            qDebug() <<__LINE__<<__FUNCTION__<< "Status Code : " << nStatusCode;
+
+            //解析返回的Json结果
+            QByteArray replyData = pReply->readAll();
+
+            // 串口调试 回复 非json格式
+
+            QJsonParseError json_error;
+            QJsonDocument jsonDoc(QJsonDocument::fromJson(replyData, &json_error));
+            if(json_error.error != QJsonParseError::NoError)
+            {
+                return false;
+            }
+            pJsonObj = jsonDoc.object();
+
+            qDebug() << "post reply: " << pJsonObj;
+            return true;
+        }
+    } else {  // 处理超时
+        QObject::disconnect(pReply, &QNetworkReply::finished, &loop, &QEventLoop::quit);
+        pReply->abort();
+        pReply->deleteLater();
+        // qDebug() << "Timeout";
+        return false;
+    }
+
+}
+bool httpclinet::post(QString api,QJsonObject &pJsonObj,QJsonObject &pJsonReply)
+{
+    QTimer timer;
+    timer.setInterval(300);  // 设置超时时间 30 秒
+    timer.setSingleShot(true);  // 单次触发
+
+    //生成对应的网络请求
+    QNetworkRequest request;
+    QString scheme = SCHEME;
+    QString serverAddr = SERVER_IP;
+    QString port = SERVER_PORT;
+    QString requestHeader = scheme + QString("://") + serverAddr + QString(":") + port;
+    QString fullRequest = requestHeader + QString(api);
+    request.setUrl(QUrl(fullRequest));
+
+    QJsonDocument body(pJsonObj);
+
+    // Connection
+    const QByteArray connection = QByteArrayLiteral("Connection");
+    const QByteArray connectionValue = QByteArrayLiteral("Connection");
+    // Content-Type
+    const QByteArray content = QByteArrayLiteral("Content-Type");
+    const QByteArray contentValue = QByteArrayLiteral("application/json;charset=UTF-8");
+    // token
+    const QByteArray token = QByteArrayLiteral("Authorization");
+    request.setHeader(QNetworkRequest::ContentTypeHeader, contentValue);
+    request.setRawHeader(connection, connectionValue);
+    request.setRawHeader(token, TOKEN);
+    QNetworkAccessManager *Manager = new QNetworkAccessManager();
+    QNetworkReply *pReply = Manager->post(request, body.toJson());
+
+    QEventLoop loop;
+    QObject::connect(&timer, &QTimer::timeout, &loop, &QEventLoop::quit);
+    QObject::connect(pReply, &QNetworkReply::finished, &loop, &QEventLoop::quit);
+    timer.start();
+    loop.exec(QEventLoop::ExcludeUserInputEvents);  // 启动事件循环
+
+    if (timer.isActive()) {  // 处理响应
+        timer.stop();
+        if (pReply->error() != QNetworkReply::NoError) {
+            // 错误处理
+            qDebug() << "Error String : " << pReply->errorString();
+            return false;
+        } else {
+            QVariant variant = pReply->attribute(QNetworkRequest::HttpStatusCodeAttribute);
+            int nStatusCode = variant.toInt();
+            // 根据状态码做进一步数据处理
+            //QByteArray bytes = pReply->readAll();
+            qDebug() <<__LINE__<<__FUNCTION__<< "Status Code : " << nStatusCode;
+
+            //解析返回的Json结果
+            QByteArray replyData = pReply->readAll();
+
+            // 串口调试 回复 非json格式
+
+            QJsonParseError json_error;
+            QJsonDocument jsonDoc(QJsonDocument::fromJson(replyData, &json_error));
+            if(json_error.error != QJsonParseError::NoError)
+            {
+                return false;
+            }
+            pJsonReply = jsonDoc.object();
+            qDebug()<<__LINE__<<__FUNCTION__<<pJsonReply<<endl;
+            return true;
+        }
+    } else {  // 处理超时
+        QObject::disconnect(pReply, &QNetworkReply::finished, &loop, &QEventLoop::quit);
+        pReply->abort();
+        pReply->deleteLater();
+        // qDebug() << "Timeout";
+        return false;
+    }
+
+}
+
+bool httpclinet::put(QString api,QJsonObject &pJsonObj,QJsonObject &pJsonReply)
+{
+    QTimer timer;
+    timer.setInterval(300);  // 设置超时时间 30 秒
+    timer.setSingleShot(true);  // 单次触发
+
+    //生成对应的网络请求
+    QNetworkRequest request;
+    QString scheme = SCHEME;
+    QString serverAddr = SERVER_IP;
+    QString port = SERVER_PORT;
+    QString requestHeader = scheme + QString("://") + serverAddr + QString(":") + port;
+    QString fullRequest = requestHeader + QString(api);
+    request.setUrl(QUrl(fullRequest));
+
+    QJsonDocument body(pJsonObj);
+
+    // Connection
+    const QByteArray connection = QByteArrayLiteral("Connection");
+    const QByteArray connectionValue = QByteArrayLiteral("Connection");
+    // Content-Type
+    const QByteArray content = QByteArrayLiteral("Content-Type");
+    const QByteArray contentValue = QByteArrayLiteral("application/json;charset=UTF-8");
+    // token
+    const QByteArray token = QByteArrayLiteral("Authorization");
+    request.setHeader(QNetworkRequest::ContentTypeHeader, contentValue);
+    request.setRawHeader(connection, connectionValue);
+    request.setRawHeader(token, TOKEN);
+    QNetworkAccessManager *Manager = new QNetworkAccessManager();
+    QNetworkReply *pReply = Manager->put(request, body.toJson());
+
+    QEventLoop loop;
+    QObject::connect(&timer, &QTimer::timeout, &loop, &QEventLoop::quit);
+    QObject::connect(pReply, &QNetworkReply::finished, &loop, &QEventLoop::quit);
+    timer.start();
+    loop.exec(QEventLoop::ExcludeUserInputEvents);  // 启动事件循环
+
+    if (timer.isActive()) {  // 处理响应
+        timer.stop();
+        if (pReply->error() != QNetworkReply::NoError) {
+            // 错误处理
+            qDebug() << "Error String : " << pReply->errorString();
+            return false;
+        } else {
+            QVariant variant = pReply->attribute(QNetworkRequest::HttpStatusCodeAttribute);
+            int nStatusCode = variant.toInt();
+            // 根据状态码做进一步数据处理
+            //QByteArray bytes = pReply->readAll();
+            qDebug() <<__LINE__<<__FUNCTION__<< "Status Code : " << nStatusCode;
+
+            //解析返回的Json结果
+            QByteArray replyData = pReply->readAll();
+
+            // 串口调试 回复 非json格式
+
+            QJsonParseError json_error;
+            QJsonDocument jsonDoc(QJsonDocument::fromJson(replyData, &json_error));
+            if(json_error.error != QJsonParseError::NoError)
+            {
+                return false;
+            }
+            pJsonReply = jsonDoc.object();
+            qDebug()<<__LINE__<<__FUNCTION__<<pJsonReply<<endl;
+            return true;
+        }
+    } else {  // 处理超时
+        QObject::disconnect(pReply, &QNetworkReply::finished, &loop, &QEventLoop::quit);
+        pReply->abort();
+        pReply->deleteLater();
+        // qDebug() << "Timeout";
+        return false;
+    }
+}
+
+
+bool httpclinet::put(QString api,QJsonObject &pJsonObj)
+{
+    QTimer timer;
+    timer.setInterval(300);  // 设置超时时间 30 秒
+    timer.setSingleShot(true);  // 单次触发
+
+    //生成对应的网络请求
+    QNetworkRequest request;
+    QString scheme = SCHEME;
+    QString serverAddr = SERVER_IP;
+    QString port = SERVER_PORT;
+    QString requestHeader = scheme + QString("://") + serverAddr + QString(":") + port;
+    QString fullRequest = requestHeader + QString(api);
+    request.setUrl(QUrl(fullRequest));
+
+    QJsonDocument body(pJsonObj);
+
+    // Connection
+    const QByteArray connection = QByteArrayLiteral("Connection");
+    const QByteArray connectionValue = QByteArrayLiteral("Connection");
+    // Content-Type
+    const QByteArray content = QByteArrayLiteral("Content-Type");
+    const QByteArray contentValue = QByteArrayLiteral("application/json;charset=UTF-8");
+    // token
+    const QByteArray token = QByteArrayLiteral("Authorization");
+    request.setHeader(QNetworkRequest::ContentTypeHeader, contentValue);
+    request.setRawHeader(connection, connectionValue);
+    request.setRawHeader(token, TOKEN);
+    QNetworkAccessManager *Manager = new QNetworkAccessManager();
+    QNetworkReply *pReply = Manager->put(request, body.toJson());
+
+    QEventLoop loop;
+    QObject::connect(&timer, &QTimer::timeout, &loop, &QEventLoop::quit);
+    QObject::connect(pReply, &QNetworkReply::finished, &loop, &QEventLoop::quit);
+    timer.start();
+    loop.exec(QEventLoop::ExcludeUserInputEvents);  // 启动事件循环
+
+    if (timer.isActive()) {  // 处理响应
+        timer.stop();
+        if (pReply->error() != QNetworkReply::NoError) {
+            // 错误处理
+            qDebug() << "Error String : " << pReply->errorString();
+            return false;
+        } else {
+            QVariant variant = pReply->attribute(QNetworkRequest::HttpStatusCodeAttribute);
+            int nStatusCode = variant.toInt();
+            // 根据状态码做进一步数据处理
+            //QByteArray bytes = pReply->readAll();
+            qDebug() <<__LINE__<<__FUNCTION__<< "Status Code : " << nStatusCode;
+
+            //解析返回的Json结果
+            QByteArray replyData = pReply->readAll();
+
+            QJsonParseError json_error;
+            QJsonDocument jsonDoc(QJsonDocument::fromJson(replyData, &json_error));
+            if(json_error.error != QJsonParseError::NoError)
+            {
+                return false;
+            }
+            pJsonObj = jsonDoc.object();
+
+            return true;
+        }
+    } else {  // 处理超时
+        QObject::disconnect(pReply, &QNetworkReply::finished, &loop, &QEventLoop::quit);
+        pReply->abort();
+        pReply->deleteLater();
+        // qDebug() << "Timeout";
+        return false;
+    }
+
+    //eventLoop.exec(QEventLoop::ExcludeUserInputEvents);
+}
+
+bool httpclinet::deleteSource(QString api,QString item)
+{
+    QTimer timer;
+    timer.setInterval(300);  // 设置超时时间 30 秒
+    timer.setSingleShot(true);  // 单次触发
+
+    //生成对应的网络请求
+    QNetworkRequest request;
+    QString scheme = SCHEME;
+    QString serverAddr = SERVER_IP;
+    QString port = SERVER_PORT;
+    QString requestHeader = scheme + QString("://") + serverAddr + QString(":") + port;
+    QString fullRequest = requestHeader + QString(api);
+    request.setUrl(QUrl(fullRequest+item));
+
+    // Connection
+    const QByteArray connection = QByteArrayLiteral("Connection");
+    const QByteArray connectionValue = QByteArrayLiteral("Connection");
+    // Content-Type
+    const QByteArray content = QByteArrayLiteral("Content-Type");
+    const QByteArray contentValue = QByteArrayLiteral("application/json;charset=UTF-8");
+    // token
+    const QByteArray token = QByteArrayLiteral("Authorization");
+    request.setHeader(QNetworkRequest::ContentTypeHeader, contentValue);
+    request.setRawHeader(connection, connectionValue);
+    request.setRawHeader(token, TOKEN);
+    QNetworkAccessManager *Manager = new QNetworkAccessManager();
+    QNetworkReply *pReply = Manager->deleteResource(request);
+
+    QEventLoop loop;
+    QObject::connect(&timer, &QTimer::timeout, &loop, &QEventLoop::quit);
+    QObject::connect(pReply, &QNetworkReply::finished, &loop, &QEventLoop::quit);
+    timer.start();
+    loop.exec(QEventLoop::ExcludeUserInputEvents);  // 启动事件循环
+
+    if (timer.isActive()) {  // 处理响应
+        timer.stop();
+        if (pReply->error() != QNetworkReply::NoError) {
+            // 错误处理
+            qDebug() << "Error String : " << pReply->errorString();
+            return false;
+        } else {
+            QVariant variant = pReply->attribute(QNetworkRequest::HttpStatusCodeAttribute);
+            int nStatusCode = variant.toInt();
+            // 根据状态码做进一步数据处理
+            //QByteArray bytes = pReply->readAll();
+            qDebug() <<__LINE__<<__FUNCTION__<< "Status Code : " << nStatusCode;
+
+            //解析返回的Json结果
+            // QByteArray replyData = pReply->readAll();
+            return true;
+        }
+    } else {  // 处理超时
+        QObject::disconnect(pReply, &QNetworkReply::finished, &loop, &QEventLoop::quit);
+        pReply->abort();
+        pReply->deleteLater();
+        // qDebug() << "Timeout";
+        return false;
+    }
+
+    //eventLoop.exec(QEventLoop::ExcludeUserInputEvents);
+
+}
+
+/*
+
+QTimer timer;
+timer.setInterval(30000);  // 设置超时时间 30 秒
+timer.setSingleShot(true);  // 单次触发
+
+// 请求 Qt 官网
+QNetworkAccessManager manager;
+QNetworkRequest request;
+request.setUrl(QUrl("http://qt-project.org"));
+request.setRawHeader("User-Agent", "MyOwnBrowser 1.0");
+
+QNetworkReply *pReply = manager.get(request);
+
+QEventLoop loop;
+connect(&timer, &QTimer::timeout, &loop, &QEventLoop::quit);
+connect(pReply, &QNetworkReply::finished, &loop, &QEventLoop::quit);
+timer.start();
+loop.exec();  // 启动事件循环
+
+if (timer.isActive()) {  // 处理响应
+    timer.stop();
+    if (pReply->error() != QNetworkReply::NoError) {
+        // 错误处理
+        qDebug() << "Error String : " << pReply->errorString();
+    } else {
+        QVariant variant = pReply->attribute(QNetworkRequest::HttpStatusCodeAttribute);
+        int nStatusCode = variant.toInt();
+        // 根据状态码做进一步数据处理
+        //QByteArray bytes = pReply->readAll();
+        qDebug() << "Status Code : " << nStatusCode;
+    }
+} else {  // 处理超时
+    disconnect(pReply, &QNetworkReply::finished, &loop, &QEventLoop::quit);
+    pReply->abort();
+    pReply->deleteLater();
+    qDebug() << "Timeout";
+}
+*/
+
---
+++ DRCA/DeviceRCA/httpclinet.h
@@ -0,0 +1,20 @@
+#ifndef HTTPCLINET_H
+#define HTTPCLINET_H
+
+#include "common.h"
+
+class httpclinet
+{
+public:
+    httpclinet();
+
+    bool get(QString api,QJsonObject &pJsonObj);
+    bool get(QString api,QJsonArray &pJsonArray);
+    bool post(QString api,QJsonObject &pJsonObj);
+    bool post(QString api,QJsonObject &pJsonObj,QJsonObject &pJsonReply);
+    bool put(QString api,QJsonObject &pJsonObj);
+    bool put(QString api,QJsonObject &pJsonObj,QJsonObject &pJsonReply);
+    bool deleteSource(QString api,QString item);
+};
+
+#endif // HTTPCLINET_H
